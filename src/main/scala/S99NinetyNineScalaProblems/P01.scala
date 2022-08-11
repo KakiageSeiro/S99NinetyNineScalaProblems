d7661189fa64f1d7332092e2d83e7907c3908b38
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2022-08-11 11:33:32 +0900

    再起になってない場合にコンパイルエラーになるアノテーションを追加

--- src/main/scala/S99NinetyNineScalaProblems/P01.scala
+++ src/main/scala/S99NinetyNineScalaProblems/P01.scala
@@ -1,5 +1,7 @@
 package S99NinetyNineScalaProblems
 
+import scala.annotation.tailrec
+
 object P01 {
   def last(list: List[Int]): Int = {
     list.last
@@ -10,6 +12,7 @@ object P01 {
   // the end.  Scala's pattern matching makes this easy.
   // 標準的な関数型アプローチは，リストの末尾に到達するまで再帰的に処理を行うことです．
   // 末尾に当たるまでリストを再帰することです。 Scalaのパターンマッチを使えば、これは簡単です。
+  @tailrec // 再起になってないときにコンパイルエラーにしてくれる
   def lastRecursive[A](ls: List[A]): A = ls match {
     case h :: Nil  => h // 終了条件。要素が無い場合を表す
     case _ :: tail => lastRecursive(tail) // 再起呼び出し。上記以外を表す
