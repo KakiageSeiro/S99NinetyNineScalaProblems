3ebd8c4ebf50e83f08a63c117d56fdd0580cd654
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2022-08-11 11:18:22 +0900

    P01の解答を理解

--- src/main/scala/S99NinetyNineScalaProblems/P01.scala
+++ src/main/scala/S99NinetyNineScalaProblems/P01.scala
@@ -2,4 +2,15 @@ object P01 {
   def last(list: List[Int]): Int = {
     list.last
   }
+
+  // 解答
+  // The standard functional approach is to recurse down the list until we hit
+  // the end.  Scala's pattern matching makes this easy.
+  // 標準的な関数型アプローチは，リストの末尾に到達するまで再帰的に処理を行うことです．
+  // 末尾に当たるまでリストを再帰することです。 Scalaのパターンマッチを使えば、これは簡単です。
+  def lastRecursive[A](ls: List[A]): A = ls match {
+    case h :: Nil  => h // 終了条件。要素が無い場合を表す
+    case _ :: tail => lastRecursive(tail) // 再起呼び出し。上記以外を表す
+    case _         => throw new NoSuchElementException // Listの要素がそもそも無い場合
+  }
 }
--- src/test/scala/P01Spec.scala
+++ src/test/scala/P01Spec.scala
@@ -7,4 +7,13 @@ class P01Spec extends AnyFlatSpec with Diagrams with TimeLimits {
   "last関数" should "要素の最後が取得できる" in {
     assert(P01.last(List(1, 2, 3)) === 3)
   }
+
+  "lastRecursive関数" should "要素の最後が取得できる" in {
+    assert(P01.last(List(1, 2, 3)) === 3)
+  }
+  "lastRecursive関数" should "要素が無い場合Throw" in {
+    intercept[NoSuchElementException] {
+      P01.last(List())
+    }
+  }
 }
