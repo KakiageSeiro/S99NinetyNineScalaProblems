9d4ae8718ac07fd06077cc3bd04d7b3a02b432f8
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2023-01-24 21:34:21 +0900

    List#foldが畳み込みではなかった

--- src/main/scala/S99NinetyNineScalaProblems/P04.scala
+++ src/main/scala/S99NinetyNineScalaProblems/P04.scala
@@ -6,7 +6,28 @@ object P04 {
     list.length
   }
 
+  // List#foldは畳み込みではなさそう
+  // https://dev.classmethod.jp/articles/scala-differece-between-fold-and-foldleft/
+  // ↑URLから引用"あるデータ型の各データ構築子に１対１に対応する関数を引数にとり、レシーバの実際の値に応じて処理をするものです"
+  // List#foldの定義がちょっと例外的なので下記Option#foldの定義をみて学ぶ
+  // def fold[B](ifEmpty: ⇒ B)(f: (A) ⇒ B): B
+  // これはNoneの場合はifEmptyを実行、Someの場合はfを実行する。
+  // このようにsealed abstract classとcase classセットで定義されているもの一つずつに対応した処理をするものがScalaのfoldと理解すればよさそう？
   def tatamikomi(list: List[Int]): Int = {
-    list.fold(0)((x, y) => x + 1)
+    // 畳み込みではない事がわかったのでコメントアウト
+    // list.fold(0)((x, y) => x + 1)
+
+    // 真の畳み込みList#foldLeft
+    // 定義
+    // def foldLeft[B](z: B)(f: (B, A) => B): B
+    //	zが初期値になる
+    //	Aにはリストの要素が順次入ってくる
+    //  Bには最初はz、次からはfの戻り値が入ってくる
+    list.foldLeft(0)((x, y) => x + 1)
+  }
+
+  def tatamikomi2(list: List[Int]): Int = {
+    // foldleftでは初期値を第一引数にしてたけど、こちらはlistの先頭要素が初期値になる。
+    list.reduceLeft((x, y) => x + 1)
   }
 }
--- src/test/scala/S99NinetyNineScalaProblems/P04Spec.scala
+++ src/test/scala/S99NinetyNineScalaProblems/P04Spec.scala
@@ -13,4 +13,8 @@ class P04Spec extends AnyFlatSpec with Diagrams with TimeLimits {
     assert(P04.tatamikomi(List(1, 1, 2, 3, 5, 8)) == 6)
   }
 
+  "tatamikomi2" should "reduceでも要素数を取得できる" in {
+    assert(P04.tatamikomi2(List(1, 1, 2, 3, 5, 8)) == 6)
+  }
+
 }
